0d97371712b0f2f21c3b92adf6b4ac37dbc03480
papa28x4 <swissfair@yahoo.co.uk>
2023-06-19 11:04:19 +0100

    more validations

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -23,12 +23,19 @@ class ResultController extends Controller
         $time = date('Y/m/d');
         $request->validate([
             'match_date' => "bail|required|date|before_or_equal:$time",
-            'team1' => 'required|different:team2',
+            'team1' => 'required|different:team2|unique:results,team1,NULL,id,team2,' . $request->team2,
             'team2' => 'required',
             'team1_score' => 'required|numeric',
             'team2_score' => 'required|numeric',
-        ], ['match_date.before_or_equal' => 'Date cannot be in the future']);
+        ], ['match_date.before_or_equal' => 'Date cannot be in the future',
+            'team1.unique' => 'Result for this match has already been entered'
+    ]);
 
+
+   
+    // 'service_id' => 'unique:service_details,service_id,NULL,id,user_id,' . $user_id;
+
+    // 'team2' => 'required|unique:results,team2,NULL,id,team1,' . $request->team1,
         try{
             $result = Result::create([
                 'match_date' => $request->match_date,
@@ -46,14 +53,16 @@ class ResultController extends Controller
 
     // public function store(Request $request)
     // {   
-        
-    //         $request->validate([
-    //             'match_date' => 'required',
-    //             'team1' => 'required|different:team2',
-    //             'team2' => 'required',
-    //             'team1_score' => 'required',
-    //             'team2_score' => 'required',
-    //         ], ['team1.different' => 'Team 1 and Team 2 cannot be the same']);
+    //     $time = date('Y/m/d');
+    //     $request->validate([
+    //         'match_date' => "bail|required|date|before_or_equal:$time",
+    //         'team1' => 'required|different:team2',
+    //         'team2' => 'required',
+    //         'team1_score' => 'required|numeric',
+    //         'team2_score' => 'required|numeric',
+    //     ], ['match_date.before_or_equal' => 'Date cannot be in the future']);
+
+    //     try{
     //         $result = Result::create([
     //             'match_date' => $request->match_date,
     //             'team1' => $request->team1,
@@ -63,9 +72,11 @@ class ResultController extends Controller
     //         ]);
 
     //         return redirect()->route('results.index')->with('message', "League table updated");
-       
+    //     }catch(\Throwable $err){
+    //         return redirect()->route('results.index')->with('error', $err->getMessage());
+    //     }
     // }
-
+    
 }
 
 
